3ab8ef7c9c3d4ad437239a2eb5afc978035baa37
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-07 16:05:55 +0530

    Completed C#: Using Classes, Methods, Objects, and Loops

---
+++ BandAssignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_nit
+{
+    internal class BandAssignment
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of your venue?");
+            string venue = args[0];
+            Console.WriteLine(Announce(venue));
+
+        }
+        static string Announce(string value)
+        {
+            return (value + " will have bands performing tonight.");
+        }
+    }
+}
---
+++ BandAssignment2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_nit
+{
+    internal class BandAssignment
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of your venue?");
+            string venue = args[0];
+            Announce(venue);
+        }
+
+        static void Announce(string venue)
+        {
+            Console.WriteLine(venue + " will have bands performing tonight.");
+        }
+    }
+}
